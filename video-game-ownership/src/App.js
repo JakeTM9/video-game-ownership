3a0c86f5d1d47bae4df16b2898999c48025010cb
JakeTM9 <wardega.jakob@gmail.com>
2024-03-24 15:52:51 -0400

    feat: render multiple components from data

--- video-game-ownership/src/App.js
+++ video-game-ownership/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import GameCard from './components/GameCard';
+import nintendo_game_data from './data/nintendo_game_data.json';
 
 function App() {
+  console.log(nintendo_game_data);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {nintendo_game_data.map((game, index) => {
+          console.log("yo")
+          return <GameCard/>;
+        })}
+        
       </header>
+      
     </div>
   );
 }
